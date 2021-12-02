81f3ebc5045e7be990a293a7eff687473c7da545
Stefan Karpinski <stefan@karpinski.org>
2021-12-02 16:26:19 -0500

    tests: stylistic updates to helper functions

--- test/setup.jl
+++ test/setup.jl
@@ -10,24 +10,35 @@ using Test
 
 const \ = setdiff
 
-is_module(G::AbstractMatrix, S::Vector{Int}) = !isempty(S) &&
+function is_module(G::AbstractMatrix, S::Vector{Int})
+    n = checksquare(G)
+    !isempty(S) &&
     all(G[i,k] == G[j,k] && G[k,i] == G[k,j]
-        for i in S for j in S for k in axes(G,2)\S)
+        for i in S for j in S for k in 1:n if k ∉ S)
+end
 
 function all_modules(G::AbstractMatrix)
     n = checksquare(G)
-    filter!(S->1 < length(S) && is_module(G, S), collect(subsets(1:n)))
+    filter!(collect(subsets(1:n))) do S
+        1 < length(S) && is_module(G, S)
+    end
 end
 
 function all_strong_modules(G::AbstractMatrix)
     modules = all_modules(G)
-    filter(A -> all(B -> !overlap(A, B), modules), modules)
+    filter(modules) do A
+        all(modules) do B
+            !overlap(A, B)
+        end
+    end
 end
 
 function is_modular_permutation(G::AbstractMatrix, p::Vector{Int};
     modules = all_strong_modules(G))
     isempty(modules) && return true
-    diffs = map(M->diff(findall(in(M), p)), modules)
+    diffs = map(modules) do M
+        diff(findall(in(M), p))
+    end
     maximum(maximum, diffs) == 1
 end
 
@@ -36,8 +47,10 @@ findin_partition(P, S) = sort!(map(x->findfirst(X->x in X, P), S))
 function is_modular_partition(G::AbstractMatrix, P::Vector{Vector{Int}};
     modules = all_strong_modules(G))
     sort!(vcat(P...)) == collect(1:size(G,2)) || error("not a partition")
-    diffs = map(M->diff(findin_partition(P, M)), modules)
+    diffs = map(modules) do M
+        diff(findin_partition(P, M))
+    end
     maximum(maximum, diffs) == 1
 end
 
-is_tournament(G::AbstractMatrix) = G + G' + I == fill(true, size(G))
+is_tournament(G::AbstractMatrix) = all(G + G' + I .== 1)
